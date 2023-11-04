5af659717ef34d885ca23746e49b50804af1097c
Drin Bratotini <drin.bratotini@gmail.com>
2023-11-04 13:01:43 -0400

    eventListener fixed

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ const dataHolder = {
   advice: null,
 };
 
-button.addEventListener("click", (_) => {
+fetchFire();
+
+button.addEventListener("click", fetchFire);
+
+function fetchFire() {
   fetch("https://api.adviceslip.com/advice")
     .then((res) => res.json())
     .then((data) => {
@@ -16,13 +20,17 @@ button.addEventListener("click", (_) => {
       console.log(dataObject, advice, id);
       dataHolder.advice = advice;
       dataHolder.id = id;
+      addText();
     });
-});
+}
 
 // functions to add advice and #id to the dom:
 
 function addText() {
   const adviceNum = document.querySelector(".advice-num");
+  const quote = document.getElementById("advice-p");
+  quote.innerText = `"${dataHolder.advice}"`;
+  adviceNum.innerText = `ADVICE #${dataHolder.id}`;
 }
 
 console.log(dataHolder);
