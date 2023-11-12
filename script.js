508df53060521aa0a44aba2f2d8d05d1a7ae5fe1
Drin Bratotini <drin.bratotini@gmail.com>
2023-11-12 10:53:47 -0500

    fixed load bug

--- script.js
+++ script.js
@@ -2,26 +2,36 @@ let button = document.querySelector(".btn");
 console.log(button);
 
 const dataHolder = {
-  id: null,
-  advice: null,
+  id: "000",
+  advice: "Fetching advice...",
 };
 
-fetchFire();
+let initialLoad = true; // only run the addText() once upon loading app
+window.addEventListener("load", () => {
+  if (initialLoad) {
+    addText();
+    fetchFire();
+    initialLoad = false;
+  }
+});
 
 button.addEventListener("click", fetchFire);
 
-function fetchFire() {
-  fetch("https://api.adviceslip.com/advice")
-    .then((res) => res.json())
-    .then((data) => {
-      const dataObject = data.slip;
-      const advice = dataObject.advice;
-      const id = dataObject.id;
-      console.log(dataObject, advice, id);
-      dataHolder.advice = advice;
-      dataHolder.id = id;
-      addText();
-    });
+async function fetchFire() {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+
+    const dataObject = data.slip;
+    const advice = dataObject.advice;
+    const id = dataObject.id;
+    console.log(dataObject, advice, id);
+    dataHolder.advice = advice;
+    dataHolder.id = id;
+    addText();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // functions to add advice and #id to the dom:
